591ff553081f2fb9731f13d4822ba306bca69b4f
Phoenix <31894252+pheabt@users.noreply.github.com>
2023-09-15 14:04:41 +0800

    Update run_mpe.sh

--- run_mpe.sh
+++ run_mpe.sh
@@ -1,8 +1,11 @@
-python main.py --agent  ac_mlp             --use_multiprocessing &
-python main.py --agent  ac_att             --use_multiprocessing &
-python main.py --agent  tiecomm            --use_multiprocessing &
-python main.py --agent  magic              --use_multiprocessing &
-python main.py --agent  commnet            --use_multiprocessing &
-python main.py --agent  ic3net             --use_multiprocessing &
-python main.py --agent  tarmac             --use_multiprocessing &
-python main.py --agent  hiercomm_random    --use_multiprocessing &
+python  main.py --map mpe-large-spread-v4 --note 555  --agent  ac_mlp  &
+python  main.py --map mpe-large-spread-v4 --note 555  --agent  ac_att  &
+python  main.py --map mpe-large-spread-v4 --note 555  --agent  tiecomm  &
+python  main.py --map mpe-large-spread-v4 --note 555  --agent  magic  &
+python  main.py --map mpe-large-spread-v4 --note 555  --agent  commnet  &
+python  main.py --map mpe-large-spread-v4 --note 555  --agent  ic3net  &
+python  main.py --map mpe-large-spread-v4 --note 555  --agent  tarmac  &
+python  main.py --map mpe-large-spread-v4 --note 555  --agent  hiercomm_random  &
+python  main.py --map mpe-large-spread-v4 --note 555  --agent  hiercomm_competition  &
+python  main.py --map mpe-large-spread-v4 --note 555  --agent  hiercomm_cooperation  &
+
